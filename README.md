0626b3be26de0761ac7dd945edcefa54776f68f9
Josh Glugatch <joshglugatch@gmail.com>
2020-10-08 16:27:18 -0700

    create test prompts with inquirer, write answers to MD

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# README-generator
+# bleh
+    
+deop
+
+
+    
+    
--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile)
 
-function writeToFile(fileName, data) {
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Project Title:"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Description:"
+        }
+    ])
 }
 
-function init() {
+function generateMD(response){
+    return`# ${response.title}
+    
+${response.description}
 
+
+    
+    `
 }
 
-init();
+promptUser().then(function(response){
+    const markdown = generateMD(response);
+    return writeFileAsync("README.md", markdown);
+}).then(function(){
+    console.log("Markdown written.")
+}).catch(function(err){
+    console.log(err)
+})
